178ed79b0f669cfea60a68a852a8605349dca22a
Dsgnry <107482514+dsgnry@users.noreply.github.com>
2022-07-19 13:42:35 +0500

    Update files.service.js

--- server/api/v1/files/files.service.js
+++ server/api/v1/files/files.service.js
@@ -1,113 +1,88 @@
 import rimraf from "rimraf";
 import { withConnection } from "./../../../common/database.js";
 import XLSX from "xlsx";
-
 export class FilesService {
-
   constructor() { }
-
   async listFiles(
   ) {
     return new Promise(async (resolve, reject) => {
       try {
         const connection = await withConnection();
-
         if (!connection) {
           return reject({ code: 501, message: "server connection error" });
         }
-
-        connection.query('SELECT * FROM egypt', function (err, result, fields) {
+        connection.query('SELECT e.id, e.IPC, e.OEM ,e.Model ,e.Size ,e.LISS ,e.PATTERN ,e.AXLE ,e.MARKINGS ,e.TECHNOLOGIES, e.SHARE, pid.uuid FROM egypt e INNER JOIN product_uuid pid on e.PATTERN = pid.PATTERN ', function (err, result, fields) {
           if (err) return reject(err);
           return resolve(result);
         });
-
       } catch (err) {
         return reject(err);
       }
     })
   }
-
   async filterFiles(
     filter
   ) {
     return new Promise(async (resolve, reject) => {
       try {
         const connection = await withConnection();
-
         if (!connection) {
           return reject({ code: 501, message: "server connection error" });
         }
-
         connection.query(`SELECT * FROM egypt where OEM='${filter}'`, function (err, result, fields) {
           if (err) return reject(err);
           return resolve(result);
         });
-
       } catch (err) {
         return reject(err);
       }
     })
   }
-
   async uploadFile(file) {
     return new Promise(async (resolve, reject) => {
       try {
         const connection = await withConnection();
-
         if (!connection) {
           return reject({ code: 501, message: "server connection error" });
         }
-
         let fileSql = `insert into last_excel_file (name, size, type) values ('${file.originalname}', '${file.size}', '${file.mimetype}')`;
-
         connection.query(fileSql, (err, result) => {
           if (err) return reject(err);
           console.log(result);
         })
-
         let workbook = XLSX.readFile(`uploads/${file.filename}`);
         let sheet_name_list = workbook.SheetNames;
         let xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-
         rimraf('uploads/*', function () { });
-
         xlData.forEach((data) => {
           let sql = `insert into egypt (IPC, OEM, Model, Size, LISS, PATTERN, AXLE, MARKINGS, TECHNOLOGIES, SHARE) values ('${data.IPC}', '${data.OEM}', '${data.Model}', '${data.Size}', '${data.LISS}', '${data.PATTERN}', '${data.AXLE}', '${data.MARKINGS}', '${data.TECHNOLOGIES}', '${data.SHARE}')`;
-
           connection.query(sql, (err, result) => {
             if (err) return reject(err);
             console.log(result);
           })
         });
-
         return resolve({ code: 200, message: "Data inserted successfully" })
       } catch (err) {
         return reject(err);
       }
     })
   }
-
   async listlastFiles(
     ) {
       return new Promise(async (resolve, reject) => {
         try {
           const connection = await withConnection();
-  
           if (!connection) {
             return reject({ code: 501, message: "server connection error" });
           }
-  
           connection.query('SELECT * FROM last_excel_file', function (err, result) {
             if (err) return reject(err);
             return resolve(result);
           });
-  
         } catch (err) {
           return reject(err);
         }
       })
     }
-
 }
-
 export default new FilesService();
